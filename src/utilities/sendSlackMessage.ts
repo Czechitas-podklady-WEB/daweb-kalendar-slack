80d706fed296a1d4af6661fe733291e20139f41e
Filip Chalupa <chalupa.filip@gmail.com>
2023-06-01 13:42:13 +0200

    Send slack message: throw error if not ok

--- src/utilities/sendSlackMessage.ts
+++ src/utilities/sendSlackMessage.ts
@@ -3,11 +3,16 @@ import { getConfiguration } from './getConfiguration'
 export const sendSlackMessage = async (data: unknown) => {
 	const { webhookUrl } = getConfiguration()
 
-	await fetch(webhookUrl, {
+	const response = await fetch(webhookUrl, {
 		method: 'POST',
 		headers: {
 			'Content-Type': 'application/json',
 		},
 		body: JSON.stringify(data),
 	})
+
+	const text = await response.text()
+	if (text !== 'ok') {
+		throw new Error(`Slack message was not sent. "${text}"`)
+	}
 }
