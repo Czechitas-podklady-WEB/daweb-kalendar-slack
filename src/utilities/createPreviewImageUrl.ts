46bbfb97bd989f292b6fe59e5c362cefd725533b
Filip Chalupa <chalupa.filip@gmail.com>
2023-06-21 13:39:31 +0200

    Preview: increase TTL

--- src/utilities/createPreviewImageUrl.ts
+++ src/utilities/createPreviewImageUrl.ts
@@ -9,7 +9,7 @@ export const createPreviewImageUrl = (slideUrl: string) => {
 	url.searchParams.set('width', '1920')
 	url.searchParams.set('height', '1080')
 	url.searchParams.set('response_type', 'image')
-	url.searchParams.set('ttl', '864000')
+	url.searchParams.set('ttl', `${60 * 60 * 24 * 30}` /* 30 days */)
 	url.searchParams.set('url', slideUrl)
 
 	return url.toString()
