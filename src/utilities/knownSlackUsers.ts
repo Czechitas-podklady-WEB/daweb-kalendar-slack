3272650187359d74c35f3da968d91269d35ab177
Filip Chalupa <chalupa.filip@gmail.com>
2023-08-29 00:56:06 +0200

    Update known Slack users

--- src/utilities/knownSlackUsers.ts
+++ src/utilities/knownSlackUsers.ts
@@ -3,57 +3,57 @@ export const knownSlackUsers = [
 		name: 'Filip Chalupa',
 		memberId: 'U05P2K2PNHF',
 	},
-	// {
-	// 	name: 'Luděk Roleček',
-	// 	memberId: 'U04QG346QN9',
-	// },
-	// {
-	// 	name: 'Filip Jirsák',
-	// 	memberId: 'U04Q4EZJHHD',
-	// },
-	// {
-	// 	name: 'Markéta Willis',
-	// 	memberId: 'U04QCD22A86',
-	// },
-	// {
-	// 	name: 'Martin Podloucký',
-	// 	memberId: 'U04QXL03WUR',
-	// },
+	{
+		name: 'Luděk Roleček',
+		memberId: 'U05PWQNQT6D',
+	},
+	{
+		name: 'Eva Machová',
+		memberId: 'U05PWQNDS1F',
+	},
 	// {
 	// 	name: 'Veronika Rychlá',
 	// 	memberId: '',
 	// },
+	{
+		name: 'Jirka Vebr',
+		memberId: 'U05PJ5ZMQ9Y',
+	},
+	{
+		name: 'Martin Podloucký',
+		memberId: 'U05PBGGCFSS',
+	},
+	{
+		name: 'Filip Jirsák',
+		memberId: 'U05PWQNP0CR',
+	},
+	{
+		name: 'Sergej Kurbanov',
+		memberId: 'U05PWQNU45P',
+	},
+	{
+		name: 'Standa Kosáček',
+		memberId: 'U05PWQNM89F',
+	},
 	// {
 	// 	name: 'Petra Drahoňovská',
 	// 	memberId: '',
 	// },
 	// {
 	// 	name: 'Petra Smiga',
-	// 	memberId: 'U04SHSS85A4',
+	// 	memberId: '',
 	// },
 	// {
 	// 	name: 'Jakub Fišer',
-	// 	memberId: 'U04QJT81CAF',
-	// },
-	// {
-	// 	name: 'Standa Kosáček',
-	// 	memberId: 'U04QMEZFNN8',
+	// 	memberId: '',
 	// },
 	// {
 	// 	name: 'Kája Šafaříková',
-	// 	memberId: 'U04RU2ZNC0K',
-	// },
-	// {
-	// 	name: 'Sergej Kurbanov',
-	// 	memberId: 'U04QCD2EGVC',
+	// 	memberId: '',
 	// },
 	// {
 	// 	name: 'Jana Meszarosová',
 	// 	memberId: '',
-	// },
-	// {
-	// 	name: 'Jirka Vebr',
-	// 	memberId: 'U04QCD4538E',
 	// },
 ] as const satisfies ReadonlyArray<{
 	name: string
